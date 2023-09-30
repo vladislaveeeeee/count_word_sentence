4dce7add7c86d408aa51ea3dcea20dee3929ff00
vladislaveeeeee <roshkovlad2007@gmail.com>
2023-09-30 13:16:46 +0300

    Add project files.

---
+++ count_word_sentence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter sentence: ");
+            string sentence = Console.ReadLine();
+
+            int words = 0;
+            bool temp = true;
+            int c_temp = 0;
+            int lenOfSentence = sentence.Length;
+
+            for (int i = 0; i < lenOfSentence; i++)
+            {
+                temp = true;
+                c_temp++;
+                if (sentence[i] == ' ')
+                {
+                    temp = false;
+                    c_temp = 0;
+                }
+                else if (temp && c_temp == 1)
+                {
+                    words++;
+                }
+            }
+
+            Console.WriteLine("Words in the sentence = " + words);
+        }
+    }
+}
